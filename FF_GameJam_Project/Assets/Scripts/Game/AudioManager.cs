75acbd0be590ea55cd9640a5c9dcce2362aebae8
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2022-02-04 19:18:09 +0100

    Audio and scenes

--- FF_GameJam_Project/Assets/Scripts/Game/AudioManager.cs
+++ FF_GameJam_Project/Assets/Scripts/Game/AudioManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+  
+   public AudioMixer mixer;
+
+
+    string masterVolume = "VolumeMaster";
+    string effectVolume = "VolumeEffects";
+    string musicVolume = "VolumeMusic";
+
+    private void Start()
     {
-        
+
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SetMute(bool state)
     {
-        
+        if (state == true)
+        {
+            mixer.SetFloat(masterVolume, 0);
+            mixer.SetFloat(effectVolume, 0);
+            mixer.SetFloat(musicVolume, 0);
+        }
+        else
+        {
+            mixer.ClearFloat(masterVolume);
+            mixer.ClearFloat(effectVolume);
+            mixer.ClearFloat(musicVolume);
+           
+        }
     }
+
+
+
+
+
 }
