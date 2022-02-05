2a09d59caceebb27041dadf67bfe4a106800263e
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2022-02-05 13:48:13 +0100

    cameras working

--- FF_GameJam_Project/Assets/Scripts/Game/CameraManager.cs
+++ FF_GameJam_Project/Assets/Scripts/Game/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,79 +6,211 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
 
-    public Camera ourCamera;
+    public Camera ourCameraOrthographic;
+    public Camera ourCameraPerspective;
 
 
+
+
+    //orthographic
+    [Header("Orthographic Camera")]
     //Movement
-    public float cameraSpeed = 1;
-
-
-
+    public float cameraSpeedOrtho = 1;
+    public float maxXPosOrtho = 0;
+    public float maxZPosOrtho = 0;
     //Zoom
-    public float zoomSpeed = 1;
-    public float zoomLimitLow = 100;
-    public float zoomLimitHigh = 200;
+    public float zoomSpeedOrtho = 1;
+    public float zoomLimitLowOrtho = 100;
+    public float zoomLimitHighOrtho = 200;
+    private float zoomValueOrtho = 0;
 
 
-    private float zoomValue=0;
+    //perspective
+    [Header("Perspective Camera")]
+    public float cameraSensitivity = 90;
+    public float verticalSpeed = 75;
+    public float MoveSpeedPers = 75;
+    public float turboMultiplierSpeed = 3;
 
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+
+    private void Start()
+    {
+        ourCameraOrthographic.enabled = true;
+        ourCameraPerspective.enabled = false;
+
+        
+    }
 
     private void LateUpdate()
     {
-
-
-        MoveCamera();
-
-        SetCameraZoom();
-        ApplyZoomCamera();
-      
-        
-
-    }
-
-    void MoveCamera()
-    {
-        float xAxis = Input.GetAxis("Horizontal") * cameraSpeed;
-        float zAxis = Input.GetAxis("Vertical") * cameraSpeed;
-
-
-        ourCamera.transform.Translate(new Vector3(xAxis, zAxis, 0.0f));
-    }
-
-    void SetCameraZoom()
-    {
-
-        if (Input.GetAxis("Mouse ScrollWheel") < 0 && ourCamera.orthographicSize < zoomLimitHigh)
+        if (ourCameraOrthographic.enabled == true)
         {
-
-           zoomValue += zoomSpeed;
-
-           
+            MoveCameraOrth();
+            SetCameraZoomOrth();
+            ApplyCameraZoomOrth();
+        }
+        else
+        {
+            MoveCameraPers();
+            CameraRotPers();
+            
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0 && ourCamera.orthographicSize > zoomLimitLow)
-        {
-            zoomValue -= zoomSpeed;
+        SwapCameraType();
 
-           
-        }
     }
 
-    void ApplyZoomCamera()
+    void SwapCameraType()
     {
 
-        if (zoomValue != 0)
+        if (Input.GetKeyDown(KeyCode.Q)) 
         {
-            float prevVal = ourCamera.orthographicSize;
 
-            ourCamera.orthographicSize = Mathf.Lerp(ourCamera.orthographicSize, ourCamera.orthographicSize+zoomValue, zoomSpeed*Time.deltaTime);
-
-
-            zoomValue = zoomValue - (ourCamera.orthographicSize- prevVal);
-
-            if (zoomValue < 0.1 && zoomValue > -0.1)
+            if (ourCameraOrthographic.enabled == true)
             {
-                zoomValue = 0;
+                ourCameraOrthographic.enabled = false;
+                ourCameraPerspective.enabled = true;
+            }
+            else
+            {
+                ourCameraOrthographic.enabled = true;
+                ourCameraPerspective.enabled = false;
+            }
+
+        }
+
+    }
+
+    void CameraRotPers()
+    {
+
+        float yAxis = Input.GetAxis("Mouse Y");
+        float xAxis = Input.GetAxis("Mouse X");
+
+
+
+        
+        rotationX += xAxis * cameraSensitivity * Time.deltaTime;
+        rotationY += yAxis * cameraSensitivity * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+        ourCameraPerspective.transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        ourCameraPerspective.transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+    }
+
+
+    void MoveCameraPers()
+    {
+
+
+        float xAxis = Input.GetAxis("Vertical");
+        float zAxis = Input.GetAxis("Horizontal");
+
+
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)){
+            ourCameraPerspective.transform.position += ourCameraPerspective.transform.forward * MoveSpeedPers* turboMultiplierSpeed * xAxis * Time.deltaTime;
+            ourCameraPerspective.transform.position += ourCameraPerspective.transform.right * MoveSpeedPers* turboMultiplierSpeed * zAxis * Time.deltaTime;
+        }
+        else {
+            ourCameraPerspective.transform.position += ourCameraPerspective.transform.forward * MoveSpeedPers * xAxis * Time.deltaTime;
+            ourCameraPerspective.transform.position += ourCameraPerspective.transform.right * MoveSpeedPers * zAxis * Time.deltaTime; 
+        }
+
+        if (ourCameraPerspective.transform.position.y < 35)
+        {
+            ourCameraPerspective.transform.position = new Vector3(ourCameraPerspective.transform.position.x, 35,ourCameraPerspective.transform.position.z);
+        }
+
+       
+
+        
+        if (Input.GetKey(KeyCode.Z)) { 
+            ourCameraPerspective.transform.position += ourCameraPerspective.transform.up * verticalSpeed * Time.deltaTime; 
+        }
+        else if (Input.GetKey(KeyCode.X)) { 
+            ourCameraPerspective.transform.position -= ourCameraPerspective.transform.up * verticalSpeed * Time.deltaTime; 
+        }
+
+    }
+
+ 
+
+
+    //ORTHOPEDIC
+    void MoveCameraOrth()
+    {
+        float xAxis = Input.GetAxis("Horizontal") * cameraSpeedOrtho * Time.deltaTime;
+
+        if (xAxis != 0)
+        {
+            if (ourCameraOrthographic.transform.position.z < maxXPosOrtho && xAxis > 0)
+            {
+                ourCameraOrthographic.transform.Translate(new Vector3(xAxis, 0.0f, 0.0f));
+            }
+            else if (ourCameraOrthographic.transform.position.z > -maxXPosOrtho && xAxis < 0)
+            {
+                ourCameraOrthographic.transform.Translate(new Vector3(xAxis, 0.0f, 0.0f));
+            }
+        }
+       
+
+        float zAxis = Input.GetAxis("Vertical") * cameraSpeedOrtho * Time.deltaTime;
+
+        if (zAxis != 0)
+        {
+
+           if (ourCameraOrthographic.transform.position.x < maxZPosOrtho && zAxis < 0)
+           {
+               ourCameraOrthographic.transform.Translate(new Vector3(0.0f, zAxis, 0.0f));
+           
+           }
+           else if (ourCameraOrthographic.transform.position.x > -maxZPosOrtho && zAxis > 0)
+           {
+               ourCameraOrthographic.transform.Translate(new Vector3(0.0f, zAxis, 0.0f));
+           }
+        }
+
+
+
+    }
+
+    void SetCameraZoomOrth()
+    {
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && ourCameraOrthographic.orthographicSize < zoomLimitHighOrtho)
+        {
+
+            zoomValueOrtho += zoomSpeedOrtho;
+
+           
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && ourCameraOrthographic.orthographicSize > zoomLimitLowOrtho)
+        {
+            zoomValueOrtho -= zoomSpeedOrtho;
+
+           
+        }
+    }
+
+    void ApplyCameraZoomOrth()
+    {
+
+        if (zoomValueOrtho != 0)
+        {
+            float prevVal = ourCameraOrthographic.orthographicSize;
+
+            ourCameraOrthographic.orthographicSize = Mathf.Lerp(ourCameraOrthographic.orthographicSize, ourCameraOrthographic.orthographicSize+ zoomValueOrtho, zoomSpeedOrtho * Time.deltaTime);
+
+
+            zoomValueOrtho = zoomValueOrtho - (ourCameraOrthographic.orthographicSize- prevVal);
+
+            if (zoomValueOrtho < 0.1 && zoomValueOrtho > -0.1)
+            {
+                zoomValueOrtho = 0;
             }
         }
 
