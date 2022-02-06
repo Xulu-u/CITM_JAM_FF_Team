c45fe6b4bad87309dce17db219af7de604b42a1b
Marc Ariza <45202074+MarcArizaAlborni@users.noreply.github.com>
2022-02-06 11:31:31 +0100

    adding text manager

--- FF_GameJam_Project/Assets/Scripts/Game/UIManager.cs
+++ FF_GameJam_Project/Assets/Scripts/Game/UIManager.cs
@@ -8,6 +8,18 @@ public class UIManager : MonoBehaviour
     public Text timerText;
     public Text countText;
 
+    public GameManager manager;
+
+
+
+
+    private void Update()
+    {
+        timerText.text = manager.timePerRound.ToString();
+
+        countText.text = manager.completedTravel.ToString();
+
+    }
 
 
 
